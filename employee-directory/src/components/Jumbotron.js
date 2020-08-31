661e33635b07213f7efe85d2659ef72e2370ad43
JerryForsberg <Jerryforsberg8991@gmail.com>
2020-08-31 00:22:01 -0700

    slight styling

--- employee-directory/src/components/Jumbotron.js
+++ employee-directory/src/components/Jumbotron.js
@@ -1,11 +1,19 @@
 import React from "react";
 import EmployeeContainer from "./EmployeeContainer";
+
+const styles = {
+    fontStyles: {
+        color: "#141469",
+        fontFamily: "Playfair Display"
+    }
+};
+
 function Jumbotron() {
     return (
         <div className="jumbotron jumbotron-fluid" >
             <div className="container-fluid">
-                <h1 className="text-center">Employee Directory</h1>
-                <p className="lead text-center">Find an employee, or sort by field </p>
+                <h1 style={styles.fontStyles} className="text-center">Employee Directory</h1>
+                <p style={styles.fontStyles} className="lead text-center">Search for an employee, or sort by field </p>
                 <EmployeeContainer />
             </div>
         </div>
